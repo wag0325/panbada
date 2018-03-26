7f9045555fd2264f9163a435762f97a1db47a623
Lydia Kwag <wag0325@gmail.com>
2018-03-26 00:16:44 -0400

    Completed UpdateMe view and form submission

--- src/components/User/UpdateMe.js
+++ src/components/User/UpdateMe.js
@@ -4,21 +4,49 @@ import { Link } from 'react-router-dom'
 import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
 
-import { withStyles } from 'material-ui/styles';
+import classNames from 'classnames'
+import { withStyles } from 'material-ui/styles'
 import { CircularProgress } from 'material-ui/Progress';
 import { LinearProgress } from 'material-ui/Progress';
 import Button from 'material-ui/Button'
 import TextField from 'material-ui/TextField'
-import Dropzone from "react-dropzone";
+import { FormControl, FormHelperText } from 'material-ui/Form'
+import Avatar from 'material-ui/Avatar'
+import Paper from 'material-ui/Paper'
+
+import Dropzone from "react-dropzone"
+import { AVATAR_DEFAULT } from '../../constants'
 
 const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  formControl: {
+    display: 'block'
+  },
+  button: {
+    margin: theme.spacing.unit * 2,
+  },
   progress: {
     margin: theme.spacing.unit * 2,
   },
-   root: {
+  root: {
     flexGrow: 1,
   },
-});
+  avatar: {
+    margin: 10,
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+  },
+})
 
 class UpdateMe extends Component {
   state = {
@@ -41,6 +69,8 @@ class UpdateMe extends Component {
   }
 
   render() {
+    const { classes } = this.props
+
     if (this.props.meQuery && this.props.meQuery.loading) {
       // return <CircularProgress className={this.props.progress} size={50} />
       return (
@@ -59,56 +89,66 @@ class UpdateMe extends Component {
     
     return (
       <div>
-        <div>{me.id}</div>
-        {this.state.firstName}
-        {this.state.lastName}
-        {this.state.avatar_url}
+      <Paper className={classes.root} elevation={4}>
         <form className={this.props.container} noValidate autoComplete="off">
-        <div>{this.props.meQuery.me.firstName}</div>
-        <TextField
-          id="firstName"
-          label="First Name"
-          className={this.props.textField}
-          value={this.state.firstName || ''}
-          onChange={e => this.setState({ firstName: e.target.value })}
-          margin="normal"
-        />
-        <TextField
-          id="lastName"
-          label="Last Name"
-          className={this.props.textField}
-          value={this.state.lastName || ''}
-          onChange={e => this.setState({ lastName: e.target.value })}
-          margin="normal"
-        />
-        <TextField
-          id="avatar_url"
-          label="Avatar"
-          className={this.props.textField}
-          value={this.state.avatar_url || ''}
-          onChange={e => this.setState({ avatar_url: e.target.value })}
-          margin="normal"
-        />
-        <TextField
-          id="curr_password"
-          label="Current Password"
-          className={this.props.textField}
-          value={this.state.oldPassword}
-          onChange={e => this.setState({ oldPassword: e.target.value })}
-          margin="normal"
-        />
-        <TextField
-          id="new_password"
-          label="New Password"
-          className={this.props.textField}
-          value={this.state.newPassword}
-          onChange={e => this.setState({ newPassword: e.target.value })}
-          margin="normal"
-        />
-        <Button variant="raised" color="primary" className={this.props.button} onClick={() => this._confirm()}>
-            Update
-        </Button>
-      </form>
+          <FormControl className={classes.formControl}>
+            <TextField
+              id="firstName"
+              label="First Name"
+              className={this.props.textField}
+              value={this.state.firstName || ''}
+              onChange={e => this.setState({ firstName: e.target.value })}
+              margin="normal"
+            />
+            <TextField
+              id="lastName"
+              label="Last Name"
+              className={this.props.textField}
+              value={this.state.lastName || ''}
+              onChange={e => this.setState({ lastName: e.target.value })}
+              margin="normal"
+            />
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <Avatar
+            alt={`${me.firstName}-${me.lastName}`}
+            src={me.avatar_url || AVATAR_DEFAULT}
+            className={classNames(classes.avatar, classes.bigAvatar)}
+            />
+            <TextField
+              id="avatar_url"
+              label="Avatar"
+              className={this.props.textField}
+              value={this.state.avatar_url || ''}
+              onChange={e => this.setState({ avatar_url: e.target.value })}
+              margin="normal"
+            />
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <TextField
+              id="curr_password"
+              label="Current Password"
+              className={this.props.textField}
+              value={this.state.oldPassword}
+              onChange={e => this.setState({ oldPassword: e.target.value })}
+              margin="normal"
+            />
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <TextField
+              id="new_password"
+              label="New Password"
+              className={this.props.textField}
+              value={this.state.newPassword}
+              onChange={e => this.setState({ newPassword: e.target.value })}
+              margin="normal"
+            />
+          </FormControl>
+          <Button variant="raised" color="primary" className={this.props.button} onClick={() => this._confirm()}>
+              Update
+          </Button>
+        </form>
+      </Paper>
       </div>
     )
   }
@@ -151,7 +191,7 @@ const ME_QUERY = gql`
       id
       firstName
       lastName
-      avatar_url
+      avatarURL
     }
   }
 `
@@ -160,20 +200,20 @@ const UPDATE_ME_MUTATION = gql`
   mutation UpdateMeMutation(
     $firstName: String, 
     $lastName: String, 
-    $avatar_url: String,
+    $avatarURL: String,
     $oldPassword: String,
     $newPassword: String) {
     updateMe(
       firstName: $firstName,
       lastName: $lastName,
-      avatar_url: $avatar_url,
+      avatarURL: $avatarURL,
       oldPassword: $oldPassword,
       newPassword: $newPassword
       ) {
       id
       firstName
       lastName
-      avatar_url
+      avatarURL
     }
   }
 `

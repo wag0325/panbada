eb6a281a007bd2461706662f7d541a6630a541ab
Lydia Kwag <wag0325@gmail.com>
2018-04-04 20:36:03 -0400

    Fixed camelize function

--- src/utils/stringFunctions.js
+++ src/utils/stringFunctions.js
@@ -1,5 +1,5 @@
 export function camelize(str) {
-	return str.split(' ').map(function(word){
+	return str.toString().split(' ').map(function(word){
     return word.charAt(0).toUpperCase() + word.slice(1)
   }).join('')
 }

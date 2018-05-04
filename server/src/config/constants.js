a4f5b52663f14e94e609facf71a6c9deb0605862
Lydia Kwag <wag0325@gmail.com>
2018-05-04 13:20:12 -0400

    Updated mongodb url

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,6 +1,7 @@
 export default {
   PORT: process.env.PORT || 8000,
-  DB_URL: 'mongodb://localhost/tweet-development',
+  DB_URL_DEV: 'mongodb://localhost/tweet-development',
+  DB_URL: 'mongodb://lydia:Panmongodb!@localhost:8000/my_db_name',
   GRAPHQL_PATH: '/graphql',
   JWT_SECRET: 'thisisasecret123',
 };
